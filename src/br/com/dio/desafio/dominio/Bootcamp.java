e680b5e866e5f517f462210bcaf2082a2c9ece84
Emerson2310 <emerson-fx@hotmail.com>
2024-07-24 22:21:09 -0300

    Projeto de POO

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,59 @@
+package br.com.dio.desafio.dominio;
+
+import java.net.ConnectException;
+import java.time.LocalDate;
+import java.util.*;
+
+public class Bootcamp {
+
+    private String nome;
+    private String descricao;
+    private final LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private final Set<Dev> devInscritos = new HashSet<>();
+    private final Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getDevInscritos() {
+        return devInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Bootcamp bootcamp)) return false;
+        return Objects.equals(getNome(), bootcamp.getNome()) && Objects.equals(getDescricao(), bootcamp.getDescricao()) && Objects.equals(getDataInicial(), bootcamp.getDataInicial()) && Objects.equals(getDataFinal(), bootcamp.getDataFinal()) && Objects.equals(getDevInscritos(), bootcamp.getDevInscritos()) && Objects.equals(getConteudos(), bootcamp.getConteudos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getDescricao(), getDataInicial(), getDataFinal(), getDevInscritos(), getConteudos());
+    }
+}
